0eaf298b91517be17710f7025d03df61861753b9
AlienX <aditya1115.ar@gmail.com>
2021-11-23 13:27:12 +0530

    Initialise Project

--- src/components/Data.js
+++ src/components/Data.js
@@ -8,7 +8,27 @@ export const Data = [
         answer: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Animi architecto ut, nostrum ab doloremque est cum eos ipsum beatae nesciunt nulla sint velit ipsa soluta quasi eligendi eveniet fugit totam!'
     },
     {
-        question: 'Enter question three?',
+        question: 'Enter question Four?',
+        answer: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Animi architecto ut, nostrum ab doloremque est cum eos ipsum beatae nesciunt nulla sint velit ipsa soluta quasi eligendi eveniet fugit totam!'
+    },
+    {
+        question: 'Enter question FIve?',
+        answer: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Animi architecto ut, nostrum ab doloremque est cum eos ipsum beatae nesciunt nulla sint velit ipsa soluta quasi eligendi eveniet fugit totam!'
+    },
+    {
+        question: 'Enter question Six?',
+        answer: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Animi architecto ut, nostrum ab doloremque est cum eos ipsum beatae nesciunt nulla sint velit ipsa soluta quasi eligendi eveniet fugit totam!'
+    },
+    {
+        question: 'Enter question Seven?',
+        answer: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Animi architecto ut, nostrum ab doloremque est cum eos ipsum beatae nesciunt nulla sint velit ipsa soluta quasi eligendi eveniet fugit totam!'
+    },
+    {
+        question: 'Enter question Eight?',
+        answer: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Animi architecto ut, nostrum ab doloremque est cum eos ipsum beatae nesciunt nulla sint velit ipsa soluta quasi eligendi eveniet fugit totam!'
+    },
+    {
+        question: 'Enter question Nine?',
         answer: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Animi architecto ut, nostrum ab doloremque est cum eos ipsum beatae nesciunt nulla sint velit ipsa soluta quasi eligendi eveniet fugit totam!'
     }
 ];
