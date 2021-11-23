31515d004035720e54c9a9bd2241e5b4622665f0
AlienX <aditya1115.ar@gmail.com>
2021-11-23 19:00:44 +0530

    Initialise Project

--- src/components/Mint_content.js
+++ src/components/Mint_content.js
@@ -1,13 +1,50 @@
 import React from 'react';
 import './Mint_content.css';
+import 'bootstrap/dist/css/bootstrap.css';
+import { Nav, Navbar, NavDropdown } from 'react-bootstrap'
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 
 
 function Mint_content() {
-    return (
-        <div id='mint_page'>
-            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Suscipit modi sunt, itaque, ea deleniti, sed libero fuga maiores eveniet ipsum culpa magni dolore. Quis, enim id, totam exercitationem sed odit animi eum qui veniam earum nisi, non dolorem? Porro dicta incidunt dignissimos omnis ex, rerum cupiditate, praesentium ducimus, nesciunt repellat nisi impedit deleniti voluptates assumenda quia ratione adipisci facere doloribus!</p>
-        </div>
-    )
+  return (
+    <div className='container' id='mintPage'>
+      <div className="navbar">
+        <Navbar id="glass" variant="dark"
+          fixed="top" expand="sm" collapseOnSelect>
+          <Navbar.Brand>
+            <img src={'/images/logo.jpg'} height='80px' margin-left='40px' />
+          </Navbar.Brand>
+          <Navbar.Toggle className="coloring" />
+          <Navbar.Collapse>
+            <Nav className='NavList'>
+              <Nav.Link id="HOVER" href="/"><Link to=''>Home</Link></Nav.Link>
+              <Nav.Link id="HOVER" href="#about"><Link to=''>About</Link></Nav.Link>
+              <Nav.Link id="HOVER" href="#faq"><Link to=''>FAQ</Link></Nav.Link>
+              <Nav.Link id="HOVER" href="#roadmap"><Link to=''>Roadmap</Link></Nav.Link>
+              <Nav.Link id="HOVER" href="#teams"><Link to=''>Team</Link></Nav.Link>
+              <Nav.Link id="HOVER"><Link to='/'>Mint</Link></Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+
+          <div className='connect'>
+            CONNECT
+          </div>
+        </Navbar>
+      </div>
+
+      <div className='mint_content'>
+        <p>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Animi architecto ut, nostrum ab doloremque est cum eos ipsum beatae nesciunt nulla sint velit ipsa soluta quasi eligendi eveniet fugit totam!
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Animi architecto ut, nostrum ab doloremque est cum eos ipsum beatae nesciunt nulla sint velit ipsa soluta quasi eligendi eveniet fugit totam!'
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Animi architecto ut, nostrum ab doloremque est cum eos ipsum beatae nesciunt nulla sint velit ipsa soluta quasi eligendi eveniet fugit totam!'
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Animi architecto ut, nostrum ab doloremque est cum eos ipsum beatae nesciunt nulla sint velit ipsa soluta quasi eligendi eveniet fugit totam!'
+        </p>
+      </div>
+
+      
+    </div>
+
+  )
 }
 
 export default Mint_content
